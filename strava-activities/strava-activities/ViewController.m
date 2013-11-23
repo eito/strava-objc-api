5efac1558cb16aa37d42f412a1666499d270b64c
Eric Ito <eric.ito@gmail.com>
2013-11-22 22:18:57 -0800

    add comments

--- strava-activities/strava-activities/ViewController.m
+++ strava-activities/strava-activities/ViewController.m
@@ -33,18 +33,11 @@
     
     //
     // NOTE: This is unnecessary in your real apps. This is purely to avoid committing sensitive data to github.
+    //       You should just set the values on STRVAPIClient directly.
     //
-    NSURL *clientIDURL = [[NSBundle mainBundle] URLForResource:@"clientid" withExtension:@""];
-    NSData *clientIDData = [NSData dataWithContentsOfURL:clientIDURL];
-    NSDictionary *clientIDJSON = [NSJSONSerialization JSONObjectWithData:clientIDData options:0 error:nil];
-    NSURL *clientSecretURL = [[NSBundle mainBundle] URLForResource:@"clientsecret" withExtension:@""];
-    NSData *clientSecretData = [NSData dataWithContentsOfURL:clientSecretURL];
-    NSDictionary *clientSecretJSON = [NSJSONSerialization JSONObjectWithData:clientSecretData options:0 error:nil];
-    
-    // NOTE: In your apps, just set the STRVAPIClient defaults directly.
-    //       I'm reading from a file so I don't commit my id/secret to github
-    [STRVAPIClient setClientID:[clientIDJSON valueForKey:@"client_id"]];
-    [STRVAPIClient setClientSecret:[clientSecretJSON valueForKey:@"client_secret"]];
+    //      +[STRVAPIClient setClientID:@""];
+    //      +[STRVAPIClient setClientSecret:@""];
+    [self loadAPIKeys];
 
     auth.clientID = [STRVAPIClient clientID];
     auth.clientSecret = [STRVAPIClient clientSecret];
@@ -76,6 +69,24 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)loadAPIKeys {
+    NSURL *clientIDURL = [[NSBundle mainBundle] URLForResource:@"clientid" withExtension:@""];
+    NSURL *clientSecretURL = [[NSBundle mainBundle] URLForResource:@"clientsecret" withExtension:@""];
+    
+    // NOTE: In your apps, just set the STRVAPIClient defaults directly.
+    //       I'm reading from a file so I don't commit my id/secret to github
+    if (clientSecretURL && clientIDURL) {
+        NSData *clientIDData = [NSData dataWithContentsOfURL:clientIDURL];
+        NSDictionary *clientIDJSON = [NSJSONSerialization JSONObjectWithData:clientIDData options:0 error:nil];
+        
+        NSData *clientSecretData = [NSData dataWithContentsOfURL:clientSecretURL];
+        NSDictionary *clientSecretJSON = [NSJSONSerialization JSONObjectWithData:clientSecretData options:0 error:nil];
+        
+        [STRVAPIClient setClientID:[clientIDJSON valueForKey:@"client_id"]];
+        [STRVAPIClient setClientSecret:[clientSecretJSON valueForKey:@"client_secret"]];
+    }
 }
 
 @end
